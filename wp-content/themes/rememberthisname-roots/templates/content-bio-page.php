21f47f535836620b5468ebdb1be5351d7cf734ff
Brian R. Jackson <brian@jaxzin.com>
2013-02-09 15:37:37 -0500

    Added hCard microformat. http://microformats.org/wiki/hCard

--- wp-content/themes/rememberthisname-roots/templates/content-bio-page.php
+++ wp-content/themes/rememberthisname-roots/templates/content-bio-page.php
@@ -1,22 +1,22 @@
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+  <article <?php post_class('vcard') ?> id="post-<?php the_ID(); ?>">
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <h1 class="entry-title fn"><?php the_title(); ?></h1>
         <div>
           <?php $birth = get_post_meta( get_the_ID(), 'rtn-date-of-birth', true );
                 $death = get_post_meta( get_the_ID(), 'rtn-date-of-death', true );
-                if ( $birth != '' and $death != '' ) {
-                  echo esc_html( $birth );
-                  echo '-';
-                  echo esc_html( $death );
+                if ( $birth != '' and $death != '' ) { ?>
+                  <time class="bday"><?php echo esc_html( $birth ); ?>
+                  -
+                  <time class="date-of-death"><?php echo esc_html( $death ); 
                 }
           ?>
         </div>
     </header>
-    <div class="entry-featured-image">
+    <div class="entry-featured-image photo">
       <?php print wp_get_attachment_image(get_post_thumbnail_id(), 'medium'); ?>
     </div>
-    <div class="entry-content">
+    <div class="entry-content note">
       <?php the_content(); ?>
     </div>
     <footer>
