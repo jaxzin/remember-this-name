822120b6d62693116732611956539179b7c8b9b5
Brian R. Jackson <brian@jaxzin.com>
2013-02-11 16:14:40 -0500

    Improved email content with featured image and additional info.

--- wp-content/themes/rememberthisname-roots/templates/content-random.php
+++ wp-content/themes/rememberthisname-roots/templates/content-random.php
@@ -2,7 +2,24 @@
 	$random_post = new WP_query(); 
 	$random_post->query('showposts=1&orderby=rand');
 	while ($random_post->have_posts()) : $random_post->the_post(); ?>
-    	<h1><?php the_title() ?></h1>
+    	<h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
+	    <div class="entry-featured-image photo">
+	    	<a href="<?php the_permalink() ?>">
+		    	<?php print wp_get_attachment_image(get_post_thumbnail_id(), array(150,150)); ?>
+			</a>
+	    </div>
+        <div>
+          <?php $birth = get_post_meta( get_the_ID(), 'rtn-date-of-birth', true );
+                $birth = date('F j, Y', strtotime($birth));
+                $death = get_post_meta( get_the_ID(), 'rtn-date-of-death', true );
+                $death = date('F j, Y', strtotime($death));
+                if ( $birth != '' and $death != '' ) { ?>
+                  <time class="bday"><?php echo esc_html( $birth ); ?>
+                  -
+                  <time class="date-of-death"><?php echo esc_html( $death ); 
+                }
+          ?>
+        </div>
         <div>
         	<?php the_content(); ?>
         </div>
